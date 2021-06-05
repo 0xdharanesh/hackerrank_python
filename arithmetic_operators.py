2a71b88de3a365bc0b8dde4177d77292007bfbe8
dharanesh <68406497+dabeet-lucifer@users.noreply.github.com>
2021-06-06 00:52:20 +0530

    Delete arithmetic_operators.py

--- arithmetic_operators.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print(a + b)
-    print(a-b)
-    print(a*b)
-
-    
-#https://github.com/dabeet-lucifer/hackerrank_python
