866acf1a3c1b817287272d903c0e96d41447a01e
dharanesh <68406497+dabeet-lucifer@users.noreply.github.com>
2021-06-06 02:08:57 +0530

    Create python_If-else.py
    This file is solution for "Python If-Else" [A Hackerrank problem]
    
    Task
    Given an integer,n, perform the following conditional actions:
    
    >If n is odd, print Weird
    >If n is even and in the inclusive range of to 2 to 5, print Not Weird
    >If n is even and in the inclusive range of to 6to20, print Weird
    <If n is even and greater than 20, print Not Weird
    
    Input Format
    
    A single line containing a positive integer,n.
    
    Constraints
    <<< 1 <=n<=100
    
    Output Format
    
    Print Weird if the number is weird. Otherwise, print Not Weird.
    
    Sample Input 0
    
    3
    
    Sample Output 0
    
    Weird
    
    Explanation 0
    
    n = 3
    
    n is odd and odd numbers are weird, so print Weird.
    
    Sample Input 1
    
    24
    
    Sample Output 1
    
    Not Weird
    
    Explanation 1
    
    n = 24
    
    n>20 and n is even, so it is not weird.

---
+++ python_If-else.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if name == 'main':
+    n = int(input().strip())
+    if n % 2:
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+
+        
+   #https://github.com/dabeet-lucifer/hackerrank_python
