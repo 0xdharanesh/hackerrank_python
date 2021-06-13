8402b06817d6ccb24928945de676aad6c7d5858b
dharanesh <68406497+dabeet-lucifer@users.noreply.github.com>
2021-06-14 00:52:14 +0530

    Delete find_the_runner-up_score.py

--- find_the_runner-up_score.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = (input()).split()
-    c = []
-    for x in n:
-        c.append(x)
-    res = []
-    [res.append(x) for x in c if x not in res]
-    g = sorted(res)
-    print(g[len(g)-2])
