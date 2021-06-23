f352b39c2d0e0b0704925fba93a92249bf9896f3
dharanesh <68406497+dabeet-lucifer@users.noreply.github.com>
2021-06-24 00:51:30 +0530

    Create string_validators.py
    This file is solution for "String Validators" [A Hackerrank problem]
    
    Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
    
    str.isalnum()
    This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
    
    >>> print 'ab123'.isalnum()
    True
    >>> print 'ab123#'.isalnum()
    False
    
    str.isalpha()
    This method checks if all the characters of a string are alphabetical (a-z and A-Z).
    
    >>> print 'abcD'.isalpha()
    True
    >>> print 'abcd1'.isalpha()
    False
    
    str.isdigit()
    This method checks if all the characters of a string are digits (0-9).
    
    >>> print '1234'.isdigit()
    True
    >>> print '123edsd'.isdigit()
    False
    
    str.islower()
    This method checks if all the characters of a string are lowercase characters (a-z).
    
    >>> print 'abcd123#'.islower()
    True
    >>> print 'Abcd123#'.islower()
    False
    
    str.isupper()
    This method checks if all the characters of a string are uppercase characters (A-Z).
    
    >>> print 'ABCD123#'.isupper()
    True
    >>> print 'Abcd123#'.isupper()
    False
    
    Task
    
    You are given a string S.
    Your task is to find out if the string S contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
    
    Input Format
    
    A single line containing a string S.
    
    Constraints
    
    Output Format
    
    In the first line, print True if S has any alphanumeric characters. Otherwise, print False.
    In the second line, print True if S has any alphabetical characters. Otherwise, print False.
    In the third line, print True if S has any digits. Otherwise, print False.
    In the fourth line, print True if S has any lowercase characters. Otherwise, print False.
    In the fifth line, print True if S has any uppercase characters. Otherwise, print False.
    
    Sample Input
    
    qA2
    
    Sample Output
    
    True
    True
    True
    True
    True
    
    : )

---
+++ string_validators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    s = input()
+    print (any([a.isalnum() for a in s]))
+    print (any([a.isalpha() for a in s]))
+    print (any([a.isdigit() for a in s]))
+    print (any([a.islower() for a in s]))
+    print (any([a.isupper() for a in s]))
+    
+    
+    
+#https://github.com/dabeet-lucifer/hackerrank_python
