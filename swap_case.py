85e578ea898989139cc83cbd9122277791df2fec
dharanesh <68406497+dabeet-lucifer@users.noreply.github.com>
2021-06-24 00:20:11 +0530

    Create swap_case.py
    This file is solution for "sWAP cASE" [A Hackerrank problem]
    
    Task
    
    You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
    
    For Example:
    
    Www.HackerRank.com → wWW.hACKERrANK.COM
    Pythonist 2 → pYTHONIST 2  
    
    Function Description
    
    Complete the swap_case function in the editor below.
    
    swap_case has the following parameters:
    
        string s: the string to modify
    
    Returns
    
        string: the modified string
    
    Input Format
    
    A single line containing a string s.
    
    Constraints
    
    o<len(s)<=1000
    
    Sample Input 0
    
    HackerRank.com presents "Pythonist 2".
    
    Sample Output 0
    
    hACKERrANK.COM PRESENTS "pYTHONIST 2".
    
    : )

---
+++ swap_case.py
@@ -0,0 +1,12 @@
+def swap_case(s):
+    b = s.swapcase()
+    return(b)
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+    
+    
+    
+ #https://github.com/dabeet-lucifer/hackerrank_python
