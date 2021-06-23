7dc2e6c9f51f07ec38b66fe6eb6c2fab68c3ece9
dharanesh <68406497+dabeet-lucifer@users.noreply.github.com>
2021-06-24 00:29:52 +0530

    Create string_split_and_join.py
    This file is solution for "String Split and Join" [A Hackerrank problem]
    
    Task
    
    In Python, a string can be split on a delimiter.
    
    Example:
    
    >>> a = "this is a string"
    >>> a = a.split(" ") # a is converted to a list of strings. 
    >>> print a
    ['this', 'is', 'a', 'string']
    
    Joining a string is simple:
    
    >>> a = "-".join(a)
    >>> print a
    this-is-a-string 
    
    Task
    You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
    
    Function Description
    
    Complete the split_and_join function in the editor below.
    
    split_and_join has the following parameters:
    
        string line: a string of space-separated words
    
    Returns
    
        string: the resulting string
    
    Input Format
    The one line contains a string consisting of space separated words.
    
    Sample Input
    
    this is a string   
    
    Sample Output
    
    this-is-a-string
    
    
    : )

---
+++ string_split_and_join.py
@@ -0,0 +1,12 @@
+def split_and_join(line):
+    a = line.split(" ")
+    b = "-".join(a)
+    return(b)
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
+    
+    
+    
+ #https://github.com/dabeet-lucifer/hackerrank_python
