1d0c091244ed77679ca1c36b6b122e836547dc5c
dharanesh <68406497+dabeet-lucifer@users.noreply.github.com>
2021-06-24 00:36:23 +0530

    Create what's_your_name.py
    This file is solution for "What's Your Name?" [A Hackerrank problem]
    
    Task
    
    You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
    
        Hello firstname lastname! You just delved into python.
    
    Function Description
    
    Complete the print_full_name function in the editor below.
    
    print_full_name has the following parameters:
    
        string first: the first name
        string last: the last name
    
    Prints
    
        string: 'Hello FIRST NAME LAST NAME! You just delved into python' where FIRST NAME and LAST NAME are replaced with FIRST and LAST.
    
    Input Format
    
    The first line contains the first name, and the second line contains the last name.
    
    Constraints
    
    The length of the first and last names are each ≤ 10.
    
    Sample Input 0
    
    Ross
    Taylor
    
    Sample Output 0
    
    Hello Ross Taylor! You just delved into python.
    
    Explanation 0
    
    The input read by the program is stored as a string data type. A string is a collection of characters.
    
    : )

---
+++ what's_your_name.py
@@ -0,0 +1,12 @@
+def print_full_name(a, b):
+   print(F"Hello {a} {b}! You just delved into python.")
+   return
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+    
+    
+    
+ #https://github.com/dabeet-lucifer/hackerrank_python
